53bbd9d913e5fdc1c48bc9bf97064a62fd902e81
andrey <andrey.bogdanovskiy@jointoit.com>
2018-04-25 17:44:21 +0300

    added axios library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+    componentDidMount = () => {
+        var categories = 'http://api.icndb.com/categories';
+
+        axios.get(categories)
+            .then((response) => {
+                if (response.data.type === 'success') {
+                    this.setState({
+                        categories: response.data.value
+                    });
+                }
+            });
+
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="jokesCategories">
+              <ul>
+                  {this.state.categories.map((category) => <li>{}</li>)}
+              </ul>
+          </div>
+          <div className="randomJokes">
+
+          </div>
       </div>
     );
   }
